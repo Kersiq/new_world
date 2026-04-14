0b80ffa8cd0593dacad12aec445c0786858b120c
Kersiq <kersientor@gmail.com>
2026-04-14 15:00:28 +0300

    include db_provider into dishka

--- src/di/__init__.py
+++ src/di/__init__.py
@@ -1,6 +1,12 @@
 from dishka import make_async_container, AsyncContainer
 
+from src.di.persistence.db import DBProvider
+
 
 async def get_async_container() -> AsyncContainer:
 
-        return make_async_container()
+        db_provider = DBProvider()
+
+        return make_async_container(
+                db_provider,
+        )
