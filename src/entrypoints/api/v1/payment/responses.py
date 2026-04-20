af4372c58a71dbce215e35a7eb9ec7c55cd2fd1e
Kersiq <kersientor@gmail.com>
2026-04-20 22:59:17 +0300

    add PaymentStatusEnum

--- src/entrypoints/api/v1/payment/responses.py
+++ src/entrypoints/api/v1/payment/responses.py
@@ -2,6 +2,8 @@ from pydantic import BaseModel
 
 from datetime import datetime
 
+from src.core.enums import PaymentStatusEnum
+
 class PostPaymentResponse(BaseModel):
     payment_id: str
     status: PaymentStatusEnum
