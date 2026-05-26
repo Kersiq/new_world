e477170b744fcb1f4f7f206fdc26b4612c6f41fa
Kersiq <kesiqq@gmail.com>
2026-05-26 14:41:10 +0300

    update i_rmq.py

--- src/application/interfaces/services/i_rmq.py
+++ src/application/interfaces/services/i_rmq.py
@@ -4,5 +4,10 @@ from typing import Any
 
 class IRMQService(ABC):
     @abstractmethod
-    async def publish(self, message: Any, queue: str) -> None:
+    async def publish(
+        self,
+        message: Any,
+        routing_key: str,
+        exchange: str | None = None,
+    ) -> None:
         raise NotImplementedError
