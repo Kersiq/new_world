8a565783cfde0476c6e6d6fc5ff73414959ac52d
Kersiq <kesiqq@gmail.com>
2026-05-26 13:46:17 +0300

    fix interface

--- src/application/interfaces/payment.py
+++ src/application/interfaces/payment.py
@@ -1,16 +1,15 @@
 from abc import ABC, abstractmethod
 
 from src.entities.payment import PaymentEntity
-from src.infra.postgres.payment.dto import PaymentInfoDTO
 from src.application.command.payment import CreatePaymentCommand
 
 
 class IPaymentRepo(ABC):
 
     @abstractmethod
-    async def get_by_idempotency_key(self, idempotency_key) -> PaymentInfoDTO | None:
+    async def get_by_idempotency_key(self, idempotency_key: str) -> PaymentEntity | None:
         raise NotImplementedError
 
     @abstractmethod
-    async def create(self, cmd: CreatePaymentCommand) -> PaymentEntity | None:
+    async def create(self, cmd: CreatePaymentCommand) -> PaymentEntity:
         raise NotImplementedError
